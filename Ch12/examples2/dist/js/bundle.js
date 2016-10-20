2f429bcdc0e3af07a9647237061226fcd51828dc
kdchang <kdchang.cc@gmail.com>
2016-10-20 19:21:22 +0800

    add examples

--- Ch12/examples2/dist/js/bundle.js
+++ Ch12/examples2/dist/js/bundle.js
@@ -1,7 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var REQUEST_URL = 'http://163.29.157.32:8080/dataset/6a3e862a-e1cb-4e44-b989-d35609559463/resource/f4a75ba9-7721-4363-884d-c3820b0b917c/download/393625397fc043188a3f8237c1da1c6f.json';
+//const REQUEST_URL = 'http://163.29.157.32:8080/dataset/6a3e862a-e1cb-4e44-b989-d35609559463/resource/f4a75ba9-7721-4363-884d-c3820b0b917c/download/393625397fc043188a3f8237c1da1c6f.json';
+var REQUEST_URL = 'http://gank.io/api/data/%E7%A6%8F%E5%88%A9/100/1';
 var data = '';
 var str = '';
 
@@ -9,14 +10,14 @@ function fetchDemo() {
   fetch(REQUEST_URL).then(function (response) {
     return response.json();
   }).then(function (json) {
-    data = json;
-    console.log(data);
+    data = json.results;
+    console.log(data.results);
     dataList();
   });
 }
 
 var dataList = function dataList() {
-  str += '\n    <img src=' + data[1].ImageName + '>\n  ';
+  str += '\n    <img src=' + data[0].url + '>\n  ';
   document.querySelector('#show-img-area').innerHTML = str;
 };
 
--- Ch12/examples2/src/scripts/main.js
+++ Ch12/examples2/src/scripts/main.js
@@ -1,4 +1,5 @@
-const REQUEST_URL = 'http://163.29.157.32:8080/dataset/6a3e862a-e1cb-4e44-b989-d35609559463/resource/f4a75ba9-7721-4363-884d-c3820b0b917c/download/393625397fc043188a3f8237c1da1c6f.json';
+//const REQUEST_URL = 'http://163.29.157.32:8080/dataset/6a3e862a-e1cb-4e44-b989-d35609559463/resource/f4a75ba9-7721-4363-884d-c3820b0b917c/download/393625397fc043188a3f8237c1da1c6f.json';
+const REQUEST_URL = 'http://gank.io/api/data/%E7%A6%8F%E5%88%A9/100/1';
 let data = '';
 let str = '';
 
@@ -6,8 +7,8 @@ function fetchDemo() {
     fetch(REQUEST_URL).then(function(response) {
         return response.json();
     }).then(function(json) {
-      data = json;
-      console.log(data);
+      data = json.results;
+      console.log(data.results);
       dataList();
     });
 }
@@ -15,7 +16,7 @@ function fetchDemo() {
 const dataList = () => {
   str += 
   `
-    <img src=${data[1].ImageName}>
+    <img src=${data[0].url}>
   `;
   document.querySelector('#show-img-area').innerHTML = str; 
 }
